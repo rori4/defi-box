ffa389b3aeb5a4374d0df1fb52952ec37cd817c8
Rangel Stoilov <rangelstoilov@gmail.com>
2020-08-19 21:59:08 +0300

    deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MyDapp = artifacts.require("MyDapp");
+const DyDxFlashLoan = artifacts.require("DyDxFlashLoan");
 
 module.exports = function (deployer) {
   deployer.deploy(MyDapp);
+  deployer.deploy(DyDxFlashLoan);
 };
